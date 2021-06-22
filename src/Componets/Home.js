c5223261aa82f34a4b2ca650e83f99c19c179f80
Tahsin Nurun <nuruntahsin@gmail.com>
2021-06-22 17:42:12 +0600

    cecking calculation logic

--- src/Componets/Home.js
+++ src/Componets/Home.js
@@ -13,9 +13,8 @@ const Home = () => {
 
     const providedName = (data) => {
         var x = {
-            A: 1, J: 1, S: 1, B: 2, K: 2, T: 2, C: 3, L: 3, U: 3, D: 4,
-            M: 4, V: 4, E: 5, N: 5, W: 5, F: 6, O: 6, X: 6, G: 7, P: 7, Y: 7, H: 8,
-            Q: 8, Z: 8, I: 9, R: 9
+            A:1, I:1, J:1, Q:1, Y:1, B:2, K:2, R:2, C:3, G:3, L:3,S:3, D:4 , M:4, T:4, E:5, H:5, N:5, X:5, U:6, V:6, W:6,
+            O:7, Z:7, F:8, P:8 
         };
         var name = data.example.replace(/\s/g, '').toUpperCase();
         var nameScore = 0;
@@ -23,6 +22,7 @@ const Home = () => {
             var curChar = name.charAt(i);
             var curValue = x[curChar];
             nameScore = nameScore + curValue;
+            console.log(nameScore);
         }
         // return nameScore;
         var output = [];
@@ -30,8 +30,15 @@ const Home = () => {
         for(var i = 0; i < sNum.length; i++) {
             output.push(+sNum.charAt(i));
         }
-        for (var i = 0, sum = 0; i < output.length; sum += output[i++]);
+        console.log(output);
+        var sum = output.reduce((a,b) => a + b);
+        // if(sum<10){
+        //     return sum
+        // }else{
+        //     sum.reduce((a,b) => a + b);
+        // }
         return sum;
+
     }
 
     const soulNum = (data, params) => {
